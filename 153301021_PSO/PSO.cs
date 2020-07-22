85a87c4e52b938ed4e9973d4252ab74ff6ae0c50
aksiustam <54524484+aksiustam@users.noreply.github.com>
2020-07-22 19:43:26 +0300

    Add files via upload

---
+++ 153301021_PSO/PSO.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _153301021_PSO
+{
+    class PSO
+    {
+
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        public static double RandomNumber(int min, int max) // Random saat tick ine göre işliyo 
+        {
+            lock (syncLock) // 
+            { // synchronize
+                return Math.Round((random.NextDouble() * (max - min) + min), Pop.penalty); // tick işlemesi çok fazla oldugundan bu kodu yazıyoz
+            } // deyse sürekli aynı sayıları elde ederiz.
+        }
+        public static Gen GenRanGen(int min, int max, int gensize)
+        {
+            var sonuc = new Gen(gensize);
+
+            for (int i = 0; i < sonuc.Genes.Length; i++)
+            {
+
+                sonuc.Genes[i] = RandomNumber(min, max);
+            }
+            return sonuc;
+        }
+
+        public static double Solution(Gen s)
+        {
+            double sonuc = 0;
+            //  (4-2.1·x1^2+x1^4/3)·x1^2+x1·x2+(-4+4·x2^2)·x2^2;
+            for (int i = 0; i < s.Genes.Length -1 ; i++)
+            {
+                double a = s.Genes[i];
+                double b = s.Genes[i+1];
+
+                double x = 4 - 2.1 * Math.Pow(a, 2) + Math.Pow(a, 4)/3;
+                x = x * Math.Pow(a, 2);
+                double y = a * b;
+                double z = -4 + 4 * Math.Pow(b, 2);
+                z = z * Math.Pow(b, 2);
+
+
+                sonuc = x + y + z;
+                
+            }
+            return Math.Round(sonuc, Pop.penalty);
+        }
+
+  
+
+        public static Pop Sort(Pop p)
+        {
+            IList<Gen> list = new List<Gen>();
+            list = p.Kromozon;
+            IEnumerable<Gen> sortedEnum = list.OrderBy(f => f.Score);
+            IList<Gen> sortedList = sortedEnum.ToList();
+            p.Kromozon = sortedList;
+            return p;
+        }
+
+        public static Pop Speed(Pop p)
+        {
+            
+            foreach (Gen h in p.Kromozon)
+            {
+                double rand1 = Math.Round(RandomNumber(0, 1),1);
+                double rand2 = Math.Round(RandomNumber(0, 1),1);
+                
+                if (h.Speed == null)
+                {
+                    h.Speed = new double[h.Genes.Length];
+                    for (int i = 0; i < h.Genes.Length; i++)
+                    {
+                        h.Speed[i] = 0;
+                    }
+                }
+
+                for (int i = 0; i < h.Genes.Length; i++)
+                {
+                    
+                    double a = h.Pbest[i] - h.Genes[i];
+                    double b = p.BestGen.Pbest[i] - h.Genes[i];
+                    double son = h.Speed[i] + (Pop.C * rand1 * a) + (Pop.C * rand2 * b);
+                    h.Speed[i] = son;
+
+                    double sonuc = h.Genes[i] + son;
+                    if (sonuc >= Pop.max) // -3 > 5
+                        sonuc = Pop.max;
+
+                    if (sonuc <= Pop.min)
+                        sonuc = Pop.min;
+                    sonuc = Math.Round(sonuc, Pop.penalty);
+                    h.Genes[i] = sonuc;
+                }
+
+                
+            }
+
+            return p;
+        }
+        
+    }
+}
---
+++ 153301021_PSO/Pop.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _153301021_PSO
+{
+    class Pop
+    {
+        public static int penalty;
+        public static int max;
+        public static int min;
+        public static int C;
+        public static int gensize;
+        
+
+        private IList<Gen> kromozon = new List<Gen>();
+        public IList<Gen> Kromozon
+        {
+            set { kromozon = value; }
+            get { return kromozon; }
+        }
+
+        private Gen _bestgen;
+        public Gen BestGen
+        {
+            set { _bestgen = value; }
+            get { return _bestgen; }
+        }
+        private int poplengt;
+        public Pop(int Poplength)
+        {
+            poplengt = Poplength;
+        }
+
+
+        public void ScoreAll()
+        {
+            
+            foreach (Gen g in kromozon) // her kromozonun genine baq
+            {
+                g.Score = PSO.Solution(g);
+                UpdatePbest(g);
+                UpdateBestGenome(g);
+            }
+            
+            
+
+        }
+
+        private void UpdateBestGenome(Gen g)
+        {
+ 
+                if (_bestgen == null || g.Pscore < _bestgen.Pscore)
+                {
+                    _bestgen = g;
+                    UpdatePbest(_bestgen);
+                }
+
+            
+        }
+
+        private void UpdatePbest(Gen g)
+        {
+
+            if (g.Pbest == null)
+            {
+                g.Pbest = new double[g.Genes.Length];
+                g.Pscore = g.Score;
+            }
+            if ( g.Score < g.Pscore) // 45 < 55
+            {
+                g.Pscore = g.Score; //  55 == 45
+                for (int i = 0; i < g.Genes.Length; i++)
+                {
+                    g.Pbest[i] = g.Genes[i];
+                }
+                    // x1 x2 = g1 g2
+            }
+           
+            
+            
+        }
+
+        public void Generate()
+        {
+
+            kromozon.Clear();
+            _bestgen = null;
+
+            for (int i = 0; i < poplengt; i++)
+            {
+                Gen genes = PSO.GenRanGen(min, max, gensize);
+                kromozon.Add(genes);
+
+            }
+        }
+
+    }
+}
